89c9230417504162dfaf211971c6a9bc799d732b
=sangeetadhiman2003 <=sangeetadhiman2003@gmail.com>
2024-01-31 15:28:06 +0530

    data enterd in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+p1 = "What's the color of apples?\n(a) Red\n(b) Pink\n(c) Orange"
+p2 = "What's the color of bananas?\n(a) Red\n(b) Pink\n(c) Yellow"
+p3 = "What's the color of pears?\n(a) Red\n(b) Green\n(c) Orange"
+
+Question.create(prompt: p1, answer: "a")
+Question.create(prompt: p2, answer: "c")
+Question.create(prompt: p3, answer: "b")
